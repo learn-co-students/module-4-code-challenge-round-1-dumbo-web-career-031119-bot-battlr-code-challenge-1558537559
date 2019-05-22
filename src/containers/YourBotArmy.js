5d9e2ebc6e1242151f7abf04bc0065e90c5a85ee
Jordan Rivera <jordan.rivera34@gmail.com>
2019-05-22 12:45:49 -0400

    Your Moves are weak

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -4,13 +4,14 @@ import BotCard from "../components/BotCard";
 class YourBotArmy extends React.Component {
   //your bot army code here...
 
+
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
             {/*...and here...*/}
-            Your Bot Army
+            
           </div>
         </div>
       </div>
