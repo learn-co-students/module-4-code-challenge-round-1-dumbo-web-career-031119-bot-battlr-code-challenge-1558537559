9d49938b29b808378e13a7165b49599697affc1c
DanFCo <danfcorreia@gmail.com>
2019-05-22 12:47:46 -0400

    bots

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -5,18 +5,24 @@ class YourBotArmy extends React.Component {
   //your bot army code here...
 
   render(){
+
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {this.props.userBotArmy.map(bot=>{
+
+
+               return <BotCard key={bot.id} bot={bot} />
+
+            })}
             Your Bot Army
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
