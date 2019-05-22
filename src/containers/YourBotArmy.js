a7618c6fa934642d4e933171e98e3ff50657780d
Daniel Ko <dankomong@gmail.com>
2019-05-22 12:37:18 -0400

    finish

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -10,7 +10,7 @@ class YourBotArmy extends React.Component {
   }
 
   render(){
-    console.log('ebots', this.props.removeEbot)
+    //console.log('ebots', this.props.removeEbot)
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
