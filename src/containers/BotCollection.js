7f0f984abf26e9e212a0eaff52d7cb074ccb6ce8
github username <github email address>
2019-05-22 11:54:15 -0400

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,12 +4,17 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
+
+renderBotCards(){
+      console.log('rendering bots')
+  return this.props.availableBots.map(bot=><BotCard handleEnlistment={this.props.handleEnlistment}bot={bot} key={bot.id} />)
+}
+
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.renderBotCards()}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,38 @@
 import React from "react";
+import BotCollection from './BotCollection.js'
+import YourBotArmy from './YourBotArmy.js'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+
+state = {
+  availableBots:[],
+  enlistedBots:[]
+}
+
+
+componentDidMount(){
+ fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+ .then(resp=>resp.json())
+ .then(bots=>this.setState({
+   availableBots:bots
+ }))
+}
+
+handleEnlistment=(newBot)=>{
+  let newAvailableBots = this.state.availableBots.filter(bot=>bot !== newBot )
+  this.setState({
+    availableBots: newAvailableBots,
+    enlistedBots:[...this.state.enlistedBots,newBot]
+  })
+}
+
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy enlistedBots={this.state.enlistedBots}/>
+        <BotCollection handleEnlistment ={this.handleEnlistment} availableBots ={this.state.availableBots}/>
       </div>
     );
   }
